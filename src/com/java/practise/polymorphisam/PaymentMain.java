fa07c8379ea2f37a1e9feedcb9bc86a4b0331b06
MURARI VAMSHARAJ <155216940+murarivamsharaj@users.noreply.github.com>
2026-05-11 22:20:57 +0530

    feat: inheritance & Polymorphisam

---
+++ src/com/java/practise/polymorphisam/PaymentMain.java
@@ -0,0 +1,11 @@
+package com.java.practise.polymorphisam;
+
+public class PaymentMain {
+    static void main() {
+        Payment pay;
+        pay = new PhonePe();
+        pay.pay();
+        pay = new GooglePay();
+        pay.pay();
+    }
+}
